f6a572aa6c6ef11ecd44ef438604e5d69fd4dbe5
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2025-02-21 17:47:49 +0530

    code splitting

--- ChatBubble/ViewController.swift
+++ ChatBubble/ViewController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    var collectionView: UICollectionView!
-        var messages: [Message] = [
-            Message(text: "Hey!", incoming:  true),
-            Message(text: "Hi! How are you?", incoming: false),
-            Message(text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", incoming:  true),
-            Message(text: "Hi!", incoming: false),
-        ]
-
+class ViewController: UIViewController {
+    var collectionView: AjoChatView!
+    var messages: [Message] = [
+        Message(text: "Hey!", incoming:  true),
+        Message(text: "Hi! How are you?", incoming: false),
+        Message(text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", incoming:  true),
+        Message(text: "Hi!", incoming: false),
+    ]
+    
     override func viewDidLoad() {
         title = "Chat"
         setupCollectionView()
@@ -23,28 +23,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func setupCollectionView(){
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.estimatedItemSize = CGSize(width: view.frame.width, height: 50)
-        
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
-        collectionView.backgroundColor = .white
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(SingleMessage.self, forCellWithReuseIdentifier: "cell")
-                
+        collectionView = AjoChatView(frame: view.bounds)
+        collectionView.messages = messages
         view.addSubview(collectionView)
     }
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return messages.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SingleMessage
-               cell.configure(with: messages[indexPath.item])
-        return cell
-    }
 }
 
 
